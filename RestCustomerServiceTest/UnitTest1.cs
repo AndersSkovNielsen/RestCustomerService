c341306d2133abc01f42b8ce102f1f520862bf54
AndersSkovNielsen <anderssnielsen@live.dk>
2018-10-10 16:00:46 +0200

    Commit

---
+++ RestCustomerServiceTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestCustomerService.Controllers;
+
+namespace RestCustomerServiceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private CustomersController controller = new CustomersController();
+
+        [TestMethod]
+        public void TestGetAll()
+        {
+            
+        }
+    }
+}
